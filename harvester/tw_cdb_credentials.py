d5128f438af8c0f13839e68e1f9a81e82b64c77e
dechengxu <dechengx@student.unimelb.edu.au>
2022-05-13 21:17:43 +1000

    add harvester

---
+++ harvester/tw_cdb_credentials.py
@@ -0,0 +1,13 @@
+consumer_key = "oqqnERGZhGdbLSC9dcBUjbpq3"
+consumer_secret = "ve6Nuf6NY4YDkwM8VTqj9e9DDAcY9pbNGzvNWmbLkFIEfvKdm4"
+access_token = "1524601630368272384-dEIU0plo2TSiBCWsicJjlLAreFe4PR"
+access_token_secret = "vMFBATuhzvnbCebMRlWN7cJ4kE1yWvEM7jGqZjAH319Ia"
+url = "http://172.26.131.86:5984/"
+login = ('admin', 'admin')
+
+#uncomment the database of the city that is gonna be searched
+#dbname = 'twitter_melbourne'
+#dbname = 'twitter_sydney'
+#dbname = 'twitter_brisbane'
+#dbname = 'twitter_perth'
+#dbname = 'twitter_adelaide'
